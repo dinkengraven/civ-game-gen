e2054eafc2ecd92c4b2d33693ee47baa1c9bdd10
dinkengraven <katelyn.dinkgrave@gmail.com>
2016-01-22 14:50:21 -0500

    place constants in separate file

---
+++ config/initializers/game_constants.rb
@@ -0,0 +1,18 @@
+CIVS = ["American", "Arabian", "Assyrian", "Austrian", "Aztec", "Babylonian", "Brazilian",
+        "Byzantine", "Carthaginian", "Celtic", "Chinese", "Danish", "Dutch", "Egyptian",
+        "English", "Ethiopian", "French", "German", "Greek", "Hunnic", "Incan", "Indian",
+        "Indonesian", "Iroquois", "Japanese", "Korean", "Mayan", "Mongolian", "Moroccan",
+        "Ottoman", "Persian", "Polish", "Polynesian", "Portuguese", "Roman", "Russian",
+        "Shoshone", "Siamese", "Songhai", "Spanish", "Swedish", "Venetian", "Zulu"]
+
+
+MAP_SIZES = ["Duel", "Tiny", "Small", "Standard", "Large", "Huge"]
+
+MAP_TYPES = ["Archipelago", "Continents", "4 Corners", "Fractal", "Great Plains", "Highlands",
+             "Ice Age", "Inland Sea", "Lakes", "North vs. South", "Oval", "Pangea", "Skirmish",
+             "Small Continents", "Terra", "Tiny Islands", "West vs. East", "Amazon Plus",
+             "Arborea", "Boreal", "Europe", "Frontier", "Great Plains Plus", "Hemispheres",
+             "Rainforest", "Tilted Axis", "Africa", "Americas", "Asia", "Bering Strait",
+             "British Isles", "Caribbean", "Continents Plus", "Earth", "Earth Plus", "Fractal",
+             "Eastern United States", "Ice Age", "Inland Sea", "Japanese Mainland", "Pangea Plus",
+             "Mediterranean", "Mesopotamia", "Ring", "Sandstorm", "Shuffle"]
