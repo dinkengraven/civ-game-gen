8b7a5320b1645f725801543e616bf2289a92876a
dinkengraven <katelyn.dinkgrave@gmail.com>
2016-01-24 14:07:02 -0500

    add method for printing rules

--- app/models/civ_game.rb
+++ app/models/civ_game.rb
@@ -1,13 +1,14 @@
 class CivGame < ActiveRecord::Base
   attr_reader :num_civs, :num_conditions, :num_city_states, :player_civ, :map_type, :map_size
-  
+
   def initialize
-		@num_civs = rand(1..21)
-    @num_conditions = rand(1..5)
-    @num_city_states = city_states
     @player_civ = CIVS.sample
+		@num_civs = rand(1..21)
+    @num_city_states = city_states
     @map_type = MAP_TYPES.sample
     @map_size = MAP_SIZES.sample
+    @num_conditions = rand(1..5)
+    @victory = set_victory_conditions
 	end
 
 	def city_states
@@ -18,11 +19,20 @@ class CivGame < ActiveRecord::Base
 		end
 	end
 
-	def victory
+	def set_victory_conditions
 		if @num_conditions == 5
 			return "All"
 		else
-			CONDITIONS.sample(@num_conditions)
+			CONDITIONS.sample(@num_conditions).join(", ")
 		end
 	end
+
+  def print_rules
+    "Player: #{@player_civ}\n
+     AI Civs: #{@num_civs}\n
+     City states: #{@num_city_states}\n
+     Map: #{@map_type}\n
+     Size: #{@map_size}\n
+     Victory: #{@victory}"
+  end
 end
