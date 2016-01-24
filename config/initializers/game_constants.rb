3fc60e96adbdd655d071c9f5ba5560fad15968db
dinkengraven <katelyn.dinkgrave@gmail.com>
2016-01-24 13:36:44 -0500

    edit strings for length

--- config/initializers/game_constants.rb
+++ config/initializers/game_constants.rb
@@ -9,12 +9,12 @@ CIVS = ["American", "Arabian", "Assyrian", "Austrian", "Aztec", "Babylonian", "B
 MAP_SIZES = ["Duel", "Tiny", "Small", "Standard", "Large", "Huge"]
 
 MAP_TYPES = ["Archipelago", "Continents", "4 Corners", "Fractal", "Great Plains", "Highlands",
-             "Ice Age", "Inland Sea", "Lakes", "North vs. South", "Oval", "Pangea", "Skirmish",
-             "Small Continents", "Terra", "Tiny Islands", "West vs. East", "Amazon Plus",
-             "Arborea", "Boreal", "Europe", "Frontier", "Great Plains Plus", "Hemispheres",
+             "Ice Age", "Inland Sea", "Lakes", "North v. South", "Oval", "Pangea", "Skirmish",
+             "Small Continents", "Terra", "Tiny Islands", "West v. East", "Amazon +",
+             "Arborea", "Boreal", "Europe", "Frontier", "Great Plains +", "Hemispheres",
              "Rainforest", "Tilted Axis", "Africa", "Americas", "Asia", "Bering Strait",
-             "British Isles", "Caribbean", "Continents Plus", "Earth", "Earth Plus", "Fractal",
-             "Eastern United States", "Ice Age", "Inland Sea", "Japanese Mainland", "Pangea Plus",
+             "British Isles", "Caribbean", "Continents +", "Earth", "Earth +", "Fractal",
+             "Eastern US", "Ice Age", "Inland Sea", "Japanese Mainland", "Pangea +",
              "Mediterranean", "Mesopotamia", "Ring", "Sandstorm", "Shuffle"]
 
 CONDITIONS = ["Domination", "Cultural", "Science", "Diplomatic", "Time"]
